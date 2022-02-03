41361369d21dc6f9ac4f11321251cb3d01a8083b
Kamen Balev <Kamen.Balev@QUALITEST.LOCAL>
2022-02-03 16:20:06 +0000

    getting page title and url

--- tests/example.test.js
+++ tests/example.test.js
@@ -29,12 +29,40 @@ describe('Interacting with Inputs', () => {
 })
 describe('Interacting with buttons', () => {
     it('should be able to click buttons', async function() {
-        const browser = await puppeteer.launch({headless: false})
+        const browser = await puppeteer.launch({headless: true})
         const page = await browser.newPage()
         await page.goto('https://devexpress.github.io/testcafe/example/')
         await page.type('#developer-name', 'steve', {delay: 0})
         await page.click('#tried-test-cafe', {clickCount: 1})
         await page.waitForTimeout(5000)
         await browser.close()
+    })
+})
+describe('Interacting with dropdowns and submitting form', () => {
+    it('should be able to select dropdown menus and submit form', async function() {
+        const browser = await puppeteer.launch({headless: true})
+        const page = await browser.newPage()
+        await page.goto('https://devexpress.github.io/testcafe/example/')
+        await page.type('#developer-name', 'steve', {delay: 0})
+        await page.click('#tried-test-cafe', {clickCount: 1})
+        await page.select('#preferred-interface', 'JavaScript API')
+        const message = 'lets fill the message with some text'
+        await page.type('#comments', message)
+        await page.click('#submit-button')
+        await page.waitForSelector('.result-content')
+        await page.waitForTimeout(5000)
+        await browser.close()
+    })
+})
+describe('Get page title and url', () =>{
+    it('should be able to get the page title and url', async function() {
+        const browser = await puppeteer.launch({headless: false, slowMo: 100, devtools: false})
+        const page = await browser.newPage()
+        await page.goto('http://example.com')
+        const title = await page.title()
+        const url = await page.url();
+        console.log(title)
+        console.log(url)
+        await browser.close();
     })
 })
